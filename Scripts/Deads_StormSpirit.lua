02678be7f67150078fa782d1cc2854d316ecda74
Deadscale <owenseal007@msn.com>
2015-05-31 15:37:22 +0100

    Update Deads_StormSpirit.lua

--- Scripts/Deads_StormSpirit.lua
+++ Scripts/Deads_StormSpirit.lua
@@ -9,11 +9,19 @@ local config = ScriptConfig.new()
 config:SetParameter("Chase", "D", config.TYPE_HOTKEY)
 config:SetParameter("Stay", "S", config.TYPE_HOTKEY)
 config:SetParameter("NoBall", "F", config.TYPE_HOTKEY)
+config:SetParameter("Distance", "E", config.TYPE_HOTKEY)
+config:SetParameter("Toggle", "X", config.TYPE_HOTKEY)
+config:SetParameter("SKeyDistance", 400)
+config:SetParameter("EKeyDistance", 650)
 config:Load()
 
 local ChaseKey = config.Chase
 local StayKey = config.Stay
 local BallKey = config.NoBall
+local DistanceKey = config.Distance
+local StayDistance = config.SKeyDistance
+local JumpDistance = config.EKeyDistance
+local toggleKey = config.Toggle
 
 -- Globals --
 local reg = false
@@ -23,14 +31,17 @@ local attack = 0
 local move = 0
 local start = false
 local reset = nil
+local active = false
 local monitor = client.screenSize.x/1600
 local F14 = drawMgr:CreateFont("F14","Tahoma",14*monitor,550*monitor) 
 local statusText = drawMgr:CreateText(-20*monitor,80*monitor,-1,"Targeting",F14) statusText.visible = false
-local me = entityList:GetMyHero()
+local toggleText  = drawMgr:CreateText(10*monitor,560*monitor,-1,"(" .. string.char(toggleKey) .. ") Hex + Silence",F14) toggleText.visible = false
+
 
 -- Load --
 function Load()
-    if PlayingGame() then
+    if PlayingGame() then  
+    local me = entityList:GetMyHero()
         if me.classId ~= CDOTA_Unit_Hero_StormSpirit then 
             script:Disable() 
         else
@@ -39,15 +50,34 @@ function Load()
             start = false
             sleep = 0
             reset = nil
+            toggleText.visible = true
+            script:RegisterEvent(EVENT_KEY,Key)
             script:RegisterEvent(EVENT_TICK,Tick)
             script:UnregisterEvent(Load)
         end
     end
 end
 
+-- Key --
+function Key(msg,code)
+	if not PlayingGame() or client.chat then return end	
+	if IsKeyDown(toggleKey) and SleepCheck("toggle") then
+		active = not active
+        Sleep(200, "toggle")
+		if active then
+			toggleText.text = "(" .. string.char(toggleKey) .. ") Hex > Silence"
+		else
+			toggleText.text = "(" .. string.char(toggleKey) .. ") Hex + Silence"
+		end
+	end
+end
+
+
 -- Main --
 function Tick(tick)
-    local attackRange = me.attackRange
+    local me = entityList:GetMyHero()
+    if not me then return end
+    local attackRange = 480
     
     if victim and victim.visible then
         if not statusText.visible then
@@ -57,7 +87,7 @@ function Tick(tick)
         statusText.visible = false
     end
     
-    if (IsKeyDown(ChaseKey) or IsKeyDown(StayKey) or IsKeyDown(BallKey))and not client.chat then
+    if (IsKeyDown(ChaseKey) or IsKeyDown(StayKey) or IsKeyDown(BallKey) or IsKeyDown(DistanceKey))and not client.chat then
         if Animations.CanMove(me) or not start or (victim and GetDistance2D(victim,me) > attackRange+450) then
             start = true
             local MouseOver = targetFind:GetLastMouseOver(1500)
@@ -79,6 +109,7 @@ function Tick(tick)
                 local SoulRing = me:FindItem("item_soul_ring")                   
                 local silenced = victim:IsSilenced() 
                 local pull = victim:IsHexed() or victim:IsStunned()
+                local disabled = victim:IsHexed() or victim:IsStunned() or victim:IsSilenced()
                 local linkens = victim:IsLinkensProtected() or victim:IsMagicImmune()
                 local distance = GetDistance2D(victim,me)                
                 local Overload = me:DoesHaveModifier("modifier_storm_spirit_overload")                
@@ -91,10 +122,9 @@ function Tick(tick)
 
                 if Q and Q:CanBeCasted() and me:CanCast() and distance <= attackRange+200 and not Overload then
                     me:CastAbility(Q)
-                    Sleep(client.latency, "casting")
                 end
                     
-                if R and R:CanBeCasted() and me:CanCast() and distance > attackRange+100 and not balling and not R.abilityphase then
+                if R and R:CanBeCasted() and me:CanCast() and distance > attackRange+200 and not balling then
                     local CP = R:FindCastPoint()
                     local delay = CP*1000+client.latency+me:GetTurnTime(victim)*1000
                     local speed = R:GetSpecialData("ball_lightning_move_speed", R.level)
@@ -107,22 +137,45 @@ function Tick(tick)
                 
                 if SoulRing and SoulRing:CanBeCasted() and distance < attackRange+200 then
                     me:CastAbility(SoulRing)
-                    Sleep(client.latency, "casting")
                 end
-                if Sheep and Sheep:CanBeCasted() and not pull and not linkens then
+                
+                if Shivas and Shivas:CanBeCasted() and distance < attackRange+200 then
+                    me:CastAbility(Shivas)
+                end
+                
+                if (Sheep and not Orchid) and Sheep:CanBeCasted() and not pull then
                     me:CastAbility(Sheep, victim)
                     Sleep(me:GetTurnTime(victim)*1000, "casting")
                 end
-                if Orchid and Orchid:CanBeCasted() and not silenced then
+                
+                if (Orchid and not Sheep) and Orchid:CanBeCasted() and not silenced then
                     me:CastAbility(Orchid, victim)
                     Sleep(me:GetTurnTime(victim)*1000, "casting")
                 end
-                if Shivas and Shivas:CanBeCasted() and distance < attackRange+200 then
-                    me:CastAbility(Shivas)
-                    Sleep(client.latency, "casting")
+                
+                if (Orchid and Sheep) and active then
+                    if Sheep:CanBeCasted() and not disabled and not linkens then
+                        me:CastAbility(Sheep, victim)
+                        Sleep(me:GetTurnTime(victim)*1000, "casting")
+                    end
+                    if (Orchid:CanBeCasted() and not Sheep:CanBeCasted()) and not disabled then
+                        me:CastAbility(Orchid, victim)
+                        Sleep(me:GetTurnTime(victim)*1000, "casting")
+                    end
+                end
+                
+                if (Orchid and Sheep) and not active then
+                    if Sheep:CanBeCasted() and not pull and not linkens then
+                        me:CastAbility(Sheep, victim)
+                        Sleep(me:GetTurnTime(victim)*1000, "casting")
+                    end
+                    if Orchid:CanBeCasted() and not silenced then
+                        me:CastAbility(Orchid, victim)
+                        Sleep(me:GetTurnTime(victim)*1000, "casting")
+                    end
                 end
                 me:Attack(victim)
-                attack = tick + 100
+                attack = Animations.maxCount/1.5
             end
         elseif tick > move and SleepCheck("casting") then
             if victim and victim.hero and not Animations.isAttacking(me) then
@@ -143,9 +196,16 @@ function Tick(tick)
                 
                 if IsKeyDown(StayKey) and R and R:CanBeCasted() and me:CanCast() and distance < attackRange+200 and not Overload then
                     local mouse = client.mousePosition
-                    local xyz = (mouse - me.position) * 400 / GetDistance2D(mouse,me) + me.position
+                    local xyz = (mouse - me.position) * StayDistance / GetDistance2D(mouse,me) + me.position
                     if GetDistance2D(me,victim) ~= 0 then
                         me:CastAbility(R,xyz)
+                        Sleep(R:FindCastPoint()*1000+me:GetTurnTime(victim)*1000, "casting")
+                    end
+                end
+                if IsKeyDown(DistanceKey) and R and R:CanBeCasted() and me:CanCast() and distance < attackRange+200 and not Overload then
+                    local position = (victim.position - me.position) * (GetDistance2D(me,victim) - (attackRange-JumpDistance)) / GetDistance2D(me,victim) + me.position
+                    if GetDistance2D(me,victim) ~= 0 then
+                        me:CastAbility(R,position)
                         Sleep(R:FindCastPoint()*1000+me:GetTurnTime(victim)*1000, "casting")
                     end
                 end
@@ -156,13 +216,15 @@ function Tick(tick)
                 elseif victim.visible and IsKeyDown(ChaseKey) then
                     local xyz = SkillShot.PredictedXYZ(victim,me:GetTurnTime(victim)*1000+client.latency+500)
                     me:Move(xyz)
+                elseif victim.visible and IsKeyDown(DistanceKey) then
+                    me:Attack(victim)
                 else
                     me:Follow(victim)
                 end
             else
                 me:Move(client.mousePosition)
             end
-            move = tick + 100
+            move = Animations.maxCount/1.5
             start = false
         end
     elseif victim then
@@ -181,9 +243,12 @@ function GameClose()
     if reg then
         reg = false
         statusText.visible = false
+        toggleText.visible = false
+        active = false
         victim = nil
         start = false
         reset = nil
+        script:UnregisterEvent(Key)
         script:UnregisterEvent(Tick)
         script:RegisterEvent(EVENT_TICK,Load)
     end
